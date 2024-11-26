1c6a800901412d6e5a7b3df203b3336924368702
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2024-11-26 16:58:16 -0600

    refactor challenge 4

--- challenge_004/src/main/kotlin/Main.kt
+++ challenge_004/src/main/kotlin/Main.kt
@@ -1,10 +1,40 @@
 package org.example
-
 import java.io.File
 
 fun main() {
     getNodes(readFile(getFilePath("nodos.txt")).toString())
 
+}
+
+fun getNodes(file: String) {
+    var nodeGroups  = file
+        .trimStart('[').trimEnd(']')
+        .split("],")
+        .map { it.trim('[', ']').split(",").toMutableSet() }
+        .toMutableList()
+    
+    var toRemove = mutableSetOf<Int>()
+
+    nodeGroups.forEachIndexed { i, currentSet ->
+        nodeGroups.forEachIndexed { j, innerSet ->
+            if (i != j && currentSet.any { it in innerSet }) {
+                currentSet.addAll(innerSet)
+                toRemove.add(j)
+            }
+        }
+    }
+    deleteElements(nodeGroups,toRemove)
+    println(getCleanedList(nodeGroups))
+}
+
+fun getCleanedList(list:MutableList<MutableSet<String>>): String {
+    return list
+        .filter { it.size < 3 }
+        .joinToString(",") { it.joinToString(",") }
+}
+
+fun deleteElements(nodeGroups:MutableList<MutableSet<String>>, toRemove: Set<Int>) {
+    toRemove.sortedDescending().forEach { nodeGroups.removeAt(it) }
 }
 
 fun readFile(path:String): List<String>{
@@ -14,29 +44,4 @@ fun readFile(path:String): List<String>{
 fun getFilePath(fileName:String):String{
     val userPath = System.getProperty("user.dir")
     return "$userPath\\src\\main\\kotlin\\$fileName"
-}
-
-fun getNodes(file: String) {
-    var newList = file.drop(3).dropLast(3).replace("[","").split("],").toMutableSet().map{ it.split(",").toMutableSet()}.toMutableList()
-    var actual: MutableSet<String>? = null
-    var deleteElements = mutableSetOf<Int>()
-    var i = 0
-    while (true){
-        actual = newList[i]
-        for (j in newList.indices){
-            if (actual.contains(newList[j].elementAt(0))
-                || actual.contains(newList[j].elementAt(1))){
-                actual.add(newList[j].elementAt(0))
-                actual.add(newList[j].elementAt(1))
-               if (i != j) deleteElements.add(j)
-            }
-        }
-         i++
-        if (i > newList.size-1) break
-    }
-    for (j in deleteElements.sorted().reversed()){
-        newList.removeAt(j)
-    }
-
-    println(newList.filter { index -> index.size < 3 }.map { it.joinToString(",")}.joinToString(","))
 }
